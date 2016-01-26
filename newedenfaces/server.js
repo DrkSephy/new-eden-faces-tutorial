05fe2d1db7afe1d07a0135dd7390aabf5f8ad62f
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-26 14:30:08 -0500

    Adding middleware to server.js

--- newedenfaces/server.js
+++ newedenfaces/server.js
@@ -23,6 +23,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res) {
+	Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
+		if (err) {
+			res.status(500).send(err.message)
+		} else if (redirectLocation) {
+			res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
+		} else if (renderProps) {
+			var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
+			var page = swig.renderFile('views/index.html', { html: html });
+		} else {
+			res.status(404).send('Page Not Found');
+		}
+	});
+});
+
 app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
