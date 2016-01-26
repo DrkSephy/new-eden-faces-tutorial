aff2ef6068064af5df30c93c18d356a38d661c5b
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-26 15:40:11 -0500

    Implemented Navbar actions

---
+++ newedenfaces/app/actions/NavbarActions.js
@@ -0,0 +1,43 @@
+import alt from '../alt';
+import {assign} from 'underscore';
+
+class NavbarActions {
+	constructor() {
+		this.generateActions(
+			'updateOnlineUsers',
+			'updateAjaxAnimation',
+			'updateSearchQuery',
+			'getCharacterCountSuccess',
+			'getCharacterCountFail',
+			'findCharacterSuccess',
+			'findCharacterFail'
+		);
+	}
+
+	findCharacter(payload) {
+		$.ajax({
+			url: '/api/characters/search',
+		})
+			.done((data) => {
+				assign(payload, data);
+				this.actions.findCharacterSuccess(payload);
+			})
+			.fail(() => {
+				this.actions.findCharacterFail(payload);
+			});
+	}
+
+	getCharacterCount() {
+		$.ajax({
+			url: '/api/characters/count' 
+		})
+			.done((data) => {
+				this.actions.getCharacterCountSuccess(data)
+			})
+			.fail((jqXhr) => {
+				this.actions.getCharacterCountFail(jqXhr)
+			});
+	}
+}
+
+export default alt.createActions(NavbarActions);
