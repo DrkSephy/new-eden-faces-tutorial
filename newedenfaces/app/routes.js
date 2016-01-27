57f21670e7761b6ff923e80b4e76aad42d211ff2
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-27 00:34:41 -0500

    Hooked up Stats route

--- newedenfaces/app/routes.js
+++ newedenfaces/app/routes.js
@@ -5,12 +5,14 @@ import Home from './components/Home';
 import AddCharacter from './components/AddCharacter';
 import Character from './components/Character';
 import CharacterList from './components/CharacterList';
+import Stats from './components/Stats';
 
 export default (
 	<Route component={App}>
 		<Route path='/' component={Home}/>
 		<Route path='/add' component={AddCharacter} />
 		<Route path='/characters/:id' component={Character} />
+		<Route path='/stats' component={Stats} />
 		<Route path=':category' component={CharacterList}>
       <Route path=':race' component={CharacterList}>
         <Route path=':bloodline' component={CharacterList} />
