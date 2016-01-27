32ef9f3f7781e385881cc7c11add54b5db6b24e0
DrkSephy <dleonar00@citymail.cuny.edu>
2016-01-27 00:19:14 -0500

    Implemented CharacterList component

--- newedenfaces/app/routes.js
+++ newedenfaces/app/routes.js
@@ -3,10 +3,12 @@ import {Route} from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import AddCharacter from './components/AddCharacter';
+import Character from './components/Character';
 
 export default (
 	<Route component={App}>
 		<Route path='/' component={Home}/>
 		<Route path='/add' component={AddCharacter} />
+		<Route path='/characters/:id' component={Character} />
 	</Route>
 );
